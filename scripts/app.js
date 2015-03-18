91ce2a04cc57bfd44ba8ba25e1e3a9d9012913eb
Miłosz Mazur <milosz.mazur@hotmail.com>
2015-03-18 17:40:56 +0100

    fix app.js, which was outdated and taken straight from build somehow

--- scripts/app.js
+++ scripts/app.js
@@ -2,7 +2,7 @@ var app = angular.module("siteApp",[
         "ngRoute"
 ]);
 
-app.config([$routeProvider, function($routeProvider) {
+app.config(function($routeProvider) {
     $routeProvider
         .when("/",{templateUrl:"views/main.html"}
              )
@@ -30,43 +30,10 @@ app.config([$routeProvider, function($routeProvider) {
              )
         .otherwise({redirectTo:"/"}
                 )
-}]);
-app.controller("MainCtrl",["$scope","$location",function(a,b) {
-    a.isActive = function(a) {return a === b.path()}
+});
+app.controller("MainCtrl",function($scope, $location) {
+    $scope.isActive = function(currLoc) {
+        return currLoc === $location.path()
+    }
 }
-]);
-
-// "use strict";
-// angular.module("siteApp",["ngCookies","ngResource","ngSanitize","ngRoute"]).config(["$routeProvider",function(a) {
-//     a.when("/",{
-//         templateUrl:"views/main.html"}
-//         ).when("/about",{
-//         templateUrl:"views/about.html"}
-//         ).when("/doc",{
-//             templateUrl:"views/doc.html"}
-//             ).when("/maps",{
-//             templateUrl:"views/maps.html"}
-//             ).when("/usecases",{
-//                 templateUrl:"views/usecases.html"}
-//                 ).when("/gs_ai",{
-//                 templateUrl:"views/gs_ai.html"}
-//                 ).when("/gs_blender",{
-//                     templateUrl:"views/gs_blender.html"}
-//                     ).when("/downloads",{
-//                     templateUrl:"views/downloads.html"}
-//                     ).when("/screens",{
-//                         templateUrl:"views/screens.html"}
-//                         ).when("/videos",{
-//                         templateUrl:"views/videos.html"}
-//                         ).when("/progression",{
-//                             templateUrl:"views/progression.html"}
-//                             ).when("/interactive_objects",{
-//                             templateUrl:"views/interactive_objects.html"}
-//                             ).otherwise({
-//                                 redirectTo:"/"}
-//                                 )}
-//                             ]),angular.module("siteApp").controller("MainCtrl",["$scope","$location",function(a,b) {
-//                                 a.awesomeThings=["HTML5 Boilerplate","AngularJS","Karma"],a.isActive = function(a) {
-//                                     return a === b.path()}
-//                             }
-//                             ]);
+);
